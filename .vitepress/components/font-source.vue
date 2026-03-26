707f83827855d8302f09300792591dee19958f70
eveningwater.123 <eveningwater.123@bytedance.com>
2026-03-26 14:53:07 +0800

    feat: 修改了查看更多按钮

--- .vitepress/components/font-source.vue
+++ .vitepress/components/font-source.vue
@@ -6,12 +6,12 @@
         <div class="pinyin-explain">
             <div class="pinyin">{{ props?.explain?.pinyin }}</div>
             <div class="explain" v-for="(item, index) in props?.explain?.explain" :key="`${item}-${index}`">
-                <div>{{ index + 1 }}.{{ item?.explain }}</div>
+                <div>{{ +index + 1 }}.{{ item?.explain }}</div>
                 <el-text size="small" v-if="item?.source">内容来源:《{{ item?.source }}》</el-text>
             </div>
-            <el-link :href="props?.explain?.more" target=“__blank” style="margin-top:10px;">
+            <el-button @click="handleClick(props?.explain?.more)" link style="margin-top:10px;" type="primary">
                 查看更多
-            </el-link>
+            </el-button>
         </div>
     </el-popover>
 </template>
@@ -33,4 +33,23 @@ const props = defineProps({
         })
     }
 })
+
+const openNewWindow = (url: string) => {
+  const a = document.createElement('a');
+  a.href = url;
+  a.target = '_blank';
+  a.rel = 'noopener noreferrer';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
+const handleClick = (url: string) => {
+    const baseUrl = window.location.origin;
+    const filterUrl = url.includes('../') ? url.replace('../','') : url.includes('./') ? url.replace('./','') : url;
+    console.log(filterUrl);
+    const firstLetter = filterUrl.slice(0,1).toUpperCase();
+    const moreUrl = `${baseUrl}/ancient-chinese-website/docs${filterUrl.includes(firstLetter) ? '' : '/' + firstLetter}/${filterUrl}`;
+    console.log(moreUrl);
+    openNewWindow(moreUrl);
+}
 </script>
